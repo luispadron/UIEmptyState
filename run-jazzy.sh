25a0bc562ae4fb14b19543e3a7af260780454cc4
Luis Padron <heyluispadron@gmail.com>
2018-04-09 13:02:10 -0400

    Move project sources to src/ directory to avoid carthage issues

--- run-jazzy.sh
+++ run-jazzy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-jazzy --clean -a Luis Padron -u https://luispadron.com -m UIEmptyState --module-version 1.0.0 --readme README.md -g https://github.com/luispadron/UIEmptyState --theme fullwidth
+jazzy --clean -a Luis Padron --source-directory src -u https://luispadron.com -m UIEmptyState --module-version 1.0.0 --readme README.md -g https://github.com/luispadron/UIEmptyState --theme fullwidth
